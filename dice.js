860de0b49245189d63c24688b088e435342ded95
Abhishek Malhotra <akshit192939@gmail.com>
2018-10-22 01:55:03 +0530

    dice

---
+++ dice.js
@@ -0,0 +1,18 @@
+let numOne = 22
+let numTwo = 7
+
+let result = 22/7
+
+// console.log(result.toFixed(2))
+
+// let floatRes = result.toFixed(2)
+
+// console.log(Math.floor(floatRes))
+// console.log(Math.ceil(floatRes))
+
+let upper = 6
+let lower = 1
+
+let randomNum =  Math.floor(Math.random() * (upper - lower + 1)) + lower
+
+console.log(randomNum)
