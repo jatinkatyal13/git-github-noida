cd4879602ad0a9c3ae1dd33550b4e91207282559
littlturtle <44334130+littlturtle@users.noreply.github.com>
2018-10-27 20:29:39 -0700

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,8 @@
 class main {
 	public static void main(String[] args) {
 		System.out.println("Hello World !");
+		System.out.println("Hello World !");
+		System.out.println("Hello World !");
+		System.out.println("Hello World !");
 	}
 }
